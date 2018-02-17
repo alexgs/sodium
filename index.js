5d94f788fc8afd0e3bc626e25c40a2c332d57bdd
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2018-02-16 19:21:08 -0500

    Add constants to exports

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const { encrypt, decrypt, key, nonce } = require('sodium-encryption/sodium');
-const sodium = { encrypt, decrypt, key, nonce };
+const { crypto_secretbox_KEYBYTES, crypto_secretbox_NONCEBYTES, crypto_secretbox_MACBYTES } = require('sodium-native');
+const sodium = {
+    encrypt,
+    decrypt,
+    key,
+    nonce,
+    KEYBYTES: crypto_secretbox_KEYBYTES,
+    MACBYTES: crypto_secretbox_MACBYTES,
+    NONCEBYTES: crypto_secretbox_NONCEBYTES
+};
 module.exports = sodium;
